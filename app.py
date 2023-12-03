92c5c54d648ba8f8b29496fe2c229d8ac8f9ad35
jhworth8 <jhworth8@gmail.com>
2023-12-03 09:06:59 -0600

    app py change?

--- app.py
+++ app.py
@@ -179,10 +179,10 @@ def search_and_download(search_term):
                 f.write(style_image_response.content)
             return style_image_path
     return None
-def load_img(path_to_img):
+def load_img(path_to_img, max_dim=400):
     img = tf.io.read_file(path_to_img)
-    if path_to_img.lower().endswith('.webp'):  # Check if the file is a WEBP
-        # Convert WEBP to JPEG in-memory
+
+    if path_to_img.lower().endswith('.webp'):  # Handle WEBP files
         with Image.open(io.BytesIO(img.numpy())) as image:
             with io.BytesIO() as jpeg_io:
                 image.convert('RGB').save(jpeg_io, 'JPEG')
@@ -191,8 +191,18 @@ def load_img(path_to_img):
 
     img = tf.image.decode_image(img, channels=3)
     img = tf.image.convert_image_dtype(img, tf.float32)
+
+    if max_dim:
+        shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+        long_dim = max(shape)
+        scale = max_dim / long_dim
+
+        new_shape = tf.cast(shape * scale, tf.int32)
+        img = tf.image.resize(img, new_shape)
+
     img = img[tf.newaxis, :]
     return img
+
 
 if __name__ == '__main__':
     app.run(debug=True)
